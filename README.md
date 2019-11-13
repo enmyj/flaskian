04ece2615835c1bb9e3f525a39739336c46a21ab
Ian <imyjer@gmail.com>
2019-11-13 00:18:56 -0500

    Actual working website

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # FlaskIan
+
+thanks to this repo for the html/css: https://github.com/engfragui/personal-website
--- flaskian/main/routes.py
+++ flaskian/main/routes.py
@@ -3,4 +3,8 @@ from flask import render_template
 
 @bp.route('/')
 def index():
-    return render_template('base.html')
+    return render_template('main.html')
+
+@bp.route('/weird')
+def weird():
+    return render_template('weird.html')
