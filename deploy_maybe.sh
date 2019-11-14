67de253bd48096c63b83a3c55b8d966c7e3fbaef
Ian Myjer <imyjer@gmail.com>
2019-11-14 10:11:00 -0500

    add docker-compose file

--- deploy_maybe.sh
+++ deploy_maybe.sh
@@ -1,23 +1,8 @@
 docker run --rm --detach \
-    --name nginx-proxy \
-    --publish 80:80 \
-    --publish 443:443 \
-    --volume /etc/nginx/certs \
-    --volume /etc/nginx/vhost.d \
-    --volume /usr/share/nginx/html \
-    --volume /var/run/docker.sock:/tmp/docker.sock:ro \
-    jwilder/nginx-proxy
-
-docker run --rm --detach \
-    --name nginx-proxy-letsencrypt \
-    --volumes-from nginx-proxy \
-    --volume /var/run/docker.sock:/var/run/docker.sock:ro \
-    --env "DEFAULT_EMAIL=imyjer@protonmail.com" \
-    jrcs/letsencrypt-nginx-proxy-companion
-
-docker run --rm --detach \
-    --name flaskian \
+    --name example-webapp \
+    --network nginx-proxy \
+    --expose 80 \
+    -e "VIRTUAL_PORT=80" \
     --env "VIRTUAL_HOST=ianmyjer.com" \
-    --env "LETSENCRYPT_EMAIL=imyjer@protonmail.com" \
     --env "LETSENCRYPT_HOST=ianmyjer.com" \
     ian:latest
